067a4456a4e8c65f7510649f4e8eae4ce7d9fe85
sandro <sandro@brunner-websites.com>
2020-04-15 16:46:02 +0200

    adding styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
+// styled component
+import { CustomButtonContainer } from './custom-button.styles'
+
+const CustomButton = ({ children, ...props }) => {
   return (
-    <button
-      className={
-        `custom-button ${isGoogleSignIn ? 'google-sign-in' : ''} ${inverted ? 'inverted' : ''}`
-      }
-      {...otherProps}>
-
+    <CustomButtonContainer {...props}>
       {children}
-
-    </button>
+    </CustomButtonContainer>
   )
 }
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,47 +1,57 @@
 import React from 'react'
-import './header.styles.scss';
-import { Link } from 'react-router-dom';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
+// styles
+import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles';
+
 // code for importing SVG logo
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+// firebase
 import { auth } from '../../firebase/firebase.utils'
 
-// Redux component
+// Redux components
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from '../../redux/user/user.selector';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
-const Header = ({ currentUser, cartHidden, toggleShowCart }) => {
+const Header = ({ currentUser, cartHidden }) => {
+
   return (
-    <div className="header">
-      <Link to="/" className="logo-container">
+
+    <HeaderContainer>
+      <LogoContainer to="/" >
         <Logo className="logo" />
-      </Link>
-      <div className="options">
+      </LogoContainer>
+      <OptionsContainer>
 
         {
           currentUser ?
-            <div className="option" onClick={() => auth.signOut()}>Sign Out</div>
+            <OptionDiv onClick={() => auth.signOut()}>
+              Sign Out
+            </OptionDiv>
             :
-            <Link to="/signin" className="option">Sign-In</Link>
+            <OptionLink to="/signin" >Sign-In</OptionLink>
         }
 
-        <Link to="/shop" className="option">Shop</Link>
-        <Link to="/contact" className="option">Contact</Link>
+        <OptionLink to="/shop" >Shop</OptionLink>
+        <OptionLink to="/contact" >Contact</OptionLink>
+
         <CartIcon />
-      </div>
+
+      </OptionsContainer>
+
       {
         cartHidden ? null :
           <CartDropdown />
       }
 
-    </div >
+    </HeaderContainer >
   )
 }
-
 
 const mapStateToProps = (state) => createStructuredSelector({
   currentUser: selectCurrentUser,
